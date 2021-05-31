ef9b2fd7b2f36e956c4070ea895961db5cb55f57
Ильнур Маматказин <ilnur.mamatkazin@gmail.com>
2021-05-31 23:51:05 +0300

    Реализация фильтра на странице Фильмы

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,68 +1,73 @@
-import React from "react";
-import Header from "../Header/Header";
-import SearchForm from "../SearchForm";
-import MoviesCardList from "../MoviesCardList";
-import Footer from "../Footer";
-import { MoviesContext } from "../../contexts";
-import "./index.css";
+import React from 'react';
+import Header from '../Header/Header';
+import SearchForm from '../SearchForm';
+import MoviesCardList from '../MoviesCardList';
+import Footer from '../Footer';
+import { MoviesContext } from '../../contexts';
+import './index.css';
 
 function Movies(props) {
   const movies = React.useContext(MoviesContext);
 
-  const [subStr, setSubStr] = React.useState("");
+  const [subStr, setSubStr] = React.useState('');
   const [shortFilm, setShortFilm] = React.useState(false);
-  const [filter, setFilter] = React.useState(movies);
+  const [filter, setFilter] = React.useState([]);
 
-  // React.useEffect(() => {
-
-  //   setFilter(result);
-  // }, [filter, subStr, shortFilm]);
+  React.useEffect(() => {
+    props.onMovies();
+  }, []);
 
   function handleSubStrChange(e) {
     setSubStr(e.target.value);
   }
 
   function handleShortFilmChange(e) {
-    setShortFilm(e.target.value);
+    setShortFilm(!shortFilm);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    console.log("&&&&", subStr, shortFilm);
+    console.log('&&&&', subStr, shortFilm, movies);
+    let result = [];
 
-    // const result = filter.reduce(
-    //   (arr, item) => {
-    //     console.log("^^^^", item);
-    //     if (item.nameRU.includes(subStr)) {
-    //       if (shortFilm && item.duration <= 40) {
-    //         return arr.push(item);
-    //       }
+    result = movies.reduce((arr, item) => {
+      console.log('^^^^', item.nameRU.includes(subStr), arr);
+      if (shortFilm) {
+        if (item.nameRU.includes(subStr) && item.duration <= 50) {
+          arr.push(item);
+        }
+      } else {
+        if (item.nameRU.includes(subStr)) {
+          arr.push(item);
+        }
+      }
 
-    //       return arr.push(item);
-    //     } else {
-    //       return arr;
-    //     }
-    //   },
-    //   [subStr, shortFilm],
-    // );
+      return arr;
+    }, []);
 
-    // setFilter(result);
+    setFilter(result);
   }
 
   return (
-    <div className="page">
-      <Header loggedIn={props.loggedIn} themeColor="light"></Header>
-      <main className="content">
-        <SearchForm onSubStrChange={handleSubStrChange} onShortFilmChange={handleShortFilmChange} />
+    <div className='page'>
+      {console.log('%%%', movies)}
+      <Header loggedIn={props.loggedIn} themeColor='light'></Header>
+      <main className='content'>
+        <SearchForm
+          onSubStrChange={handleSubStrChange}
+          onShortFilmChange={handleShortFilmChange}
+          onSubmit={handleSubmit}
+          checked={shortFilm}
+        />
         <MoviesCardList
-          cards={movies}
+          cards={filter}
           liked={false}
           // onCardClick={props.onCardClick}
           onCardLike={props.onCardLike}
           onCardDelete={props.onCardDelete}
         />
-        <button className="button button-movies">Ещё</button>
+        <button className='button button-movies'>Ещё</button>
       </main>
       <Footer />
     </div>
